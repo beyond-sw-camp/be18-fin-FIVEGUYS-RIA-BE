84e62f21d58123753e085dde77ec34d7d3598e40
amicitia3600 <dyungwoo3600@gmail.com>
2025-11-16 20:44:29 +0900

    [feat]building-zone

---
+++ src/main/java/com/fiveguys/RIA/RIA_Backend/facility/building/model/entity/Building.java
@@ -0,0 +1,54 @@
+package com.fiveguys.RIA.RIA_Backend.facility.building.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "BUILDING")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class Building {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long buildingId;
+
+  @Column(nullable = false, length = 100)
+  private String buildingName;
+
+  @Column(length = 255)
+  private String address;
+
+  @Column(length = 255)
+  private String description;
+
+  @Column(nullable = false, updatable = false)
+  private LocalDateTime createdAt;
+
+  @Column
+  private LocalDateTime updatedAt;
+
+  @PrePersist
+  public void prePersist() {
+    this.createdAt = LocalDateTime.now();
+  }
+
+  @PreUpdate
+  public void preUpdate() {
+    this.updatedAt = LocalDateTime.now();
+  }
+}
