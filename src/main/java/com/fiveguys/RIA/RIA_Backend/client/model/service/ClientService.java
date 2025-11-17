fb9723741af7ec9d14d2fffc10d4b492852a33f6
amicitia3600 <dyungwoo3600@gmail.com>
2025-11-17 21:07:55 +0900

    [refactor] type 변경및 필요없는 코드 삭제

--- src/main/java/com/fiveguys/RIA/RIA_Backend/client/model/service/ClientService.java
+++ src/main/java/com/fiveguys/RIA/RIA_Backend/client/model/service/ClientService.java
@@ -1,10 +1,8 @@
 package com.fiveguys.RIA.RIA_Backend.client.model.service;
 
 import com.fiveguys.RIA.RIA_Backend.client.model.dto.request.ClientRequestDto;
-import com.fiveguys.RIA.RIA_Backend.client.model.dto.response.ClientCompanyListPageResponseDto;
 import com.fiveguys.RIA.RIA_Backend.client.model.dto.response.ClientListResponseDto;
 import com.fiveguys.RIA.RIA_Backend.client.model.dto.response.ClientResponseDto;
-import com.fiveguys.RIA.RIA_Backend.client.model.entity.ClientCompany;
 
 public interface ClientService {
 
