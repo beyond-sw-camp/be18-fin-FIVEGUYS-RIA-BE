d64b23019fb59d5ae60dd5715c95171b90f8aa22
Jo YongJu <whwjyj00@naver.com>
2025-11-17 20:10:37 +0900

    feat: setter - builder로 변경

--- src/main/java/com/fiveguys/RIA/RIA_Backend/admin/model/dto/RoleChangeRequestDto.java
+++ src/main/java/com/fiveguys/RIA/RIA_Backend/admin/model/dto/RoleChangeRequestDto.java
@@ -1,13 +1,15 @@
 package com.fiveguys.RIA.RIA_Backend.admin.model.dto;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class RoleChangeRequestDto {
     @NotNull(message = "권한 ID는 필수입니다.")
     private Long roleId;
