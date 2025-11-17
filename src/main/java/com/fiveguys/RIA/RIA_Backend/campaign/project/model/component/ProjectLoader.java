8dc36de0a39896fef424db32b8151af9f40707d1
amicitia3600 <dyungwoo3600@gmail.com>
2025-11-17 21:07:55 +0900

    [refactor] type 변경및 필요없는 코드 삭제

--- src/main/java/com/fiveguys/RIA/RIA_Backend/campaign/project/model/component/ProjectLoader.java
+++ src/main/java/com/fiveguys/RIA/RIA_Backend/campaign/project/model/component/ProjectLoader.java
@@ -1,6 +1,5 @@
 package com.fiveguys.RIA.RIA_Backend.campaign.project.model.component;
 
-import com.fiveguys.RIA.RIA_Backend.campaign.pipeline.model.entity.Pipeline;
 import com.fiveguys.RIA.RIA_Backend.campaign.pipeline.model.repository.PipelineRepository;
 import com.fiveguys.RIA.RIA_Backend.campaign.project.model.entity.Project;
 import com.fiveguys.RIA.RIA_Backend.campaign.project.model.repository.ProjectRepository;
