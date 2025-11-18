7945af6ca41079756583dbdac920bb23909c6f0a
Jo YongJu <whwjyj00@naver.com>
2025-11-18 16:31:55 +0900

    feat: 엔티티 수정

--- src/main/java/com/fiveguys/RIA/RIA_Backend/admin/model/entity/AdminLog.java
+++ src/main/java/com/fiveguys/RIA/RIA_Backend/admin/model/entity/AdminLog.java
@@ -11,6 +11,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
@@ -18,8 +19,8 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "ADMIN_LOG")
-@AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class AdminLog {
 
     @Id
@@ -34,16 +35,16 @@ public class AdminLog {
     @Column(name = "LOG_NAME", nullable = false, length = 255)
     private String logName;
 
-    @Column(name = "USER_NAME", length = 255)
+    @Column(name = "USER_NAME", nullable = false ,length = 50)
     private String userName;
 
-    @Column(name = "EMPLOYEE_NO", length = 50, unique = true)
+    @Column(name = "EMPLOYEE_NO", length = 50)
     private String employeeNo;
 
     @Column(name = "RESOURCE", nullable = false, length = 255)
     private String resource;
 
-    @Column(name ="STATE", nullable = false, length = 20)
+    @Column(name ="STATE", length = 20)
     private String state;
 
     @Column(name = "CREATED_AT", nullable = false, updatable = false)
