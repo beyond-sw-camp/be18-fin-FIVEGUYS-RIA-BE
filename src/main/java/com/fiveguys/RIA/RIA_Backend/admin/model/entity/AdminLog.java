505dad9afd7aca8c918e640ffd69541ef0fa49d0
Jo YongJu <whwjyj00@naver.com>
2025-11-18 15:59:24 +0900

    feat: 로그 저장 테이블 엔티티 생성

---
+++ src/main/java/com/fiveguys/RIA/RIA_Backend/admin/model/entity/AdminLog.java
@@ -0,0 +1,56 @@
+package com.fiveguys.RIA.RIA_Backend.admin.model.entity;
+
+import com.fiveguys.RIA.RIA_Backend.user.model.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ADMIN_LOG")
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdminLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ADMIN_LOG_ID")
+    private Long logId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ADMIN_ID")
+    private User admin;
+
+    @Column(name = "LOG_NAME", nullable = false, length = 255)
+    private String logName;
+
+    @Column(name = "USER_NAME", length = 255)
+    private String userName;
+
+    @Column(name = "EMPLOYEE_NO", length = 50, unique = true)
+    private String employeeNo;
+
+    @Column(name = "RESOURCE", nullable = false, length = 255)
+    private String resource;
+
+    @Column(name ="STATE", nullable = false, length = 20)
+    private String state;
+
+    @Column(name = "CREATED_AT", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
