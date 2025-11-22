e3430e955f1822df3b955703efbaf63d373ca804
LWjin8 <lwj08062403@gmail.com>
2025-11-22 14:52:22 +0900

    1차 수정본

--- src/main/java/com/fiveguys/RIA/RIA_Backend/calendar/model/dto/response/CalendarResponseDto.java
+++ src/main/java/com/fiveguys/RIA/RIA_Backend/calendar/model/dto/response/CalendarResponseDto.java
@@ -1,10 +1,10 @@
 package com.fiveguys.RIA.RIA_Backend.calendar.model.dto.response;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Data
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class CalendarResponseDto {
