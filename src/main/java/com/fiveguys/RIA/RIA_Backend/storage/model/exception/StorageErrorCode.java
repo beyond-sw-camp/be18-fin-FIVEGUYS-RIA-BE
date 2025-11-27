5ae3abe712be9de36fe88948bef2508b2e5ce340
Jo YongJu <whwjyj00@naver.com>
2025-11-27 17:00:36 +0900

    release: 이모티콘 쨈

--- src/main/java/com/fiveguys/RIA/RIA_Backend/storage/model/exception/StorageErrorCode.java
+++ src/main/java/com/fiveguys/RIA/RIA_Backend/storage/model/exception/StorageErrorCode.java
@@ -12,7 +12,7 @@ public enum StorageErrorCode implements ErrorCode {
     STORAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "STO-001", "파일을 찾을 수 없습니다."),
     FORBIDDEN_DELETE(HttpStatus.FORBIDDEN, "STO-002", "이 파일을 삭제할 권한이 없습니다."),
     S3_DELETE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "STO-003", "S3 파일 삭제에 실패했습니다."),
-    FORBIDDEN_DOWNLOAD(HttpStatus.FORBIDDEN, "STO-004", "이 파일을 다운로드할 권한이 없습니다."); // ✅ 추가
+    FORBIDDEN_DOWNLOAD(HttpStatus.FORBIDDEN, "STO-004", "이 파일을 다운로드할 권한이 없습니다.");
 
     private final HttpStatus status;
     private final String code;
