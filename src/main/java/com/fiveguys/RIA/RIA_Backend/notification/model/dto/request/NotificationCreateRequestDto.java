16ac421d42fd30939cc2e34a59339392937828e9
minsu47722 <minsu47722@naver.com>
2025-11-27 12:23:24 +0900

    [feat]notification

--- src/main/java/com/fiveguys/RIA/RIA_Backend/notification/model/dto/request/NotificationCreateRequestDto.java
+++ src/main/java/com/fiveguys/RIA/RIA_Backend/notification/model/dto/request/NotificationCreateRequestDto.java
@@ -10,10 +10,10 @@ import lombok.Getter;
 @Builder
 public class NotificationCreateRequestDto {
 
-    private Long senderId;
-    private Long receiverId;
-    private NotificationTargetType targetType;
-    private NotificationTargetAction targetAction;
-    private Long targetId;
+    private final Long senderId;
+    private final Long receiverId;
+    private final NotificationTargetType targetType;
+    private final NotificationTargetAction targetAction;
+    private final Long targetId;
     private final NotificationContext context; // Type에 맞는 Key-Value를 주기 위함
 }
