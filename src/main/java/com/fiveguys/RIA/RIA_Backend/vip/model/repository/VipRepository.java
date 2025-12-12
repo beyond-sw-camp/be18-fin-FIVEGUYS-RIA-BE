5900f1840761afef985979f12b654e04598b0bf8
amicitia3600 <dyungwoo3600@gmail.com>
2025-12-12 19:21:18 +0900

    [feat] bug

--- src/main/java/com/fiveguys/RIA/RIA_Backend/vip/model/repository/VipRepository.java
+++ src/main/java/com/fiveguys/RIA/RIA_Backend/vip/model/repository/VipRepository.java
@@ -20,7 +20,7 @@ public interface VipRepository extends JpaRepository<Vip, Long> {
   // 등급 + 키워드(이름, 연락처) 검색 통합
   @Query("""
       select v
-      from vip v
+      from Vip v
       where (:grade is null or v.grade = :grade)
         and (
           :keyword is null
