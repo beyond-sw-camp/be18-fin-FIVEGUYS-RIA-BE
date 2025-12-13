8c76ee41c7c82f9b896d249087123c4ba5b24913
minsu47722 <minsu47722@naver.com>
2025-12-13 18:55:08 +0900

    fix

--- src/main/java/com/fiveguys/RIA/RIA_Backend/notification/model/service/impl/NotificationSseServiceImpl.java
+++ src/main/java/com/fiveguys/RIA/RIA_Backend/notification/model/service/impl/NotificationSseServiceImpl.java
@@ -19,9 +19,6 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @RequiredArgsConstructor
